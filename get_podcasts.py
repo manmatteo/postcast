4253617a81c1d7397719d1c0cc0741beb1bce128
Matteo Manighetti <m.manighetti@gmail.com>
2022-10-31 13:05:05 +0100

    Fix link

--- get_podcasts.py
+++ get_podcasts.py
@@ -52,7 +52,7 @@ with requests.Session() as s:
         source_soup = BeautifulSoup(resp.text, 'html.parser')
         for link in source_soup.find_all('a'):
             if (link.get('class') == ['next', 'page-numbers']):
-                podcast_pages.append(link.get('href'))
+                podcast_pages.append('https://www.ilpost.it' + link.get('href'))
             if (link.get('class') == ['play'] and link.get("data-title")):
                 new_episode_tag = out_soup.new_tag("item")
                 channel_tag.append(new_episode_tag)
